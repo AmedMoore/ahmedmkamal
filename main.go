77c736a0626cdb848bdedffec5acf0e33a48b8c6
Ahmed Kamal <info@ahmedmkamal.com>
2024-02-29 16:49:14 +0200

    KWD

--- main.go
+++ main.go
@@ -1,16 +1,107 @@
 package main
 
 import (
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/samber/lo"
 )
 
+var pgpContentPath = "./.well-known/openpgpkey"
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.File("index.html")
+	r := gin.New()
+
+	if gin.Mode() != gin.ReleaseMode {
+		r.Use(gin.Logger())
+	}
+
+	r.Use(gin.Recovery())
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.File("index.html")
 	})
-	r.GET("/.well-known/openpgpkey/info=ahmedmkamal.com/B682F878CF8BD3B7D8F58EE5CE4D80E8E6A5E533.asc", func(c *gin.Context) {
-		c.File(".well-known/openpgpkey/info=ahmedmkamal.com/B682F878CF8BD3B7D8F58EE5CE4D80E8E6A5E533.asc")
-	})
+
+	pgpEntities := getOpenPgpEntities()
+
+	openpgpkey := r.Group("/.well-known/openpgpkey")
+	{
+		openpgpkey.Use(cors.Default())
+
+		domain := openpgpkey.Group("/:domain")
+		{
+			domain.GET("/policy", func(ctx *gin.Context) {
+
+				domain := ctx.Params.ByName("domain")
+				entities := pgpEntities[domain]
+
+				if len(entities) == 0 {
+					ctx.Abort()
+					ctx.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+				} else {
+					ctx.File(knownEntityPolicyFilePath(domain))
+				}
+			})
+
+			domain.GET("/hu/:hash", func(ctx *gin.Context) {
+				domain := ctx.Params.ByName("domain")
+				hash := ctx.Params.ByName("hash")
+
+				entities := pgpEntities[domain]
+				if len(entities) == 0 || !lo.Contains(entities, hash) {
+					ctx.Abort()
+					ctx.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+				} else {
+					ctx.File(knownEntityKeyFilePath(domain, hash))
+				}
+			})
+		}
+	}
+
 	r.Run()
+}
+
+// Map of domain and name hashes
+type PgpEntities = map[string][]string
+
+func knownEntityPolicyFilePath(domain string) string {
+	return filepath.Join(pgpContentPath, domain, "policy")
+}
+
+func knownEntityKeyFilePath(domain, hash string) string {
+	return filepath.Join(pgpContentPath, domain, "hu", hash)
+}
+
+func getOpenPgpEntities() PgpEntities {
+	files, err := os.ReadDir(pgpContentPath)
+	if err != nil {
+		panic(err)
+	}
+
+	entities := make(PgpEntities)
+
+	for _, file := range files {
+		domain := file.Name()
+		entities[domain] = appendEntities(domain)
+	}
+
+	return entities
+}
+
+func appendEntities(domain string) []string {
+	files, err := os.ReadDir(filepath.Join(pgpContentPath, domain, "hu"))
+	if err != nil {
+		panic(err)
+	}
+
+	hashes := make([]string, len(files))
+
+	for idx, file := range files {
+		hashes[idx] = file.Name()
+	}
+
+	return hashes
 }
